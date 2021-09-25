5d3dff1ed968578527b57eff70298d5eca701388
Jonatan David Orozco Cantillo <jonorozcoc@gmail.com>
2021-09-25 13:52:40 -0500

    feat: added tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -2,29 +2,13 @@
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
+<main class="text-center p-4 m-0 max-w-xs sm:max-w-none">
+	<h1 class="uppercase text-red-600 text-7xl font-thin">Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
